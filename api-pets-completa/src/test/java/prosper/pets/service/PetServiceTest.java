c5e0877e3fbb8fc2c0e2eec35ac4a4fe691050ef
José Yoshiriro <jyoshiriro@gmail.com>
2024-11-20 13:30:11 -0300

    novo teste

---
+++ api-pets-completa/src/test/java/prosper/pets/service/PetServiceTest.java
@@ -0,0 +1,31 @@
+package prosper.pets.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.web.server.ResponseStatusException;
+import prosper.pets.domain.Pet;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PetServiceTest {
+
+	private PetService petService = new PetService(null, null, null);
+
+	@Test
+	void getListaOuStatus204_returnsPets_whenPetsListIsNotEmpty() {
+		List<Pet> pets = List.of(new Pet());
+		List<Pet> result = petService.getListaOuStatus204(pets);
+		assertEquals(pets, result);
+	}
+
+	@Test
+	void getListaOuStatus204_throwsNoContent_whenPetsListIsEmpty() {
+		List<Pet> pets = Collections.emptyList();
+		ResponseStatusException exception =
+								assertThrows(ResponseStatusException.class, () -> petService.getListaOuStatus204(pets));
+
+		assertEquals(204, exception.getRawStatusCode());
+	}
+}
